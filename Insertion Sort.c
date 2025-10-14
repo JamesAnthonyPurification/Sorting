b41235651589f6f89b600341741852e685ff7db1
James Anthony Purification <129299940+JamesAnthonyPurification@users.noreply.github.com>
2025-10-14 20:51:45 +0600

    Update Insertion Sort.c

--- Insertion Sort.c
+++ Insertion Sort.c
@@ -13,7 +13,7 @@ int main() {
        for (int i = 1; i < n; i++) {
         int temp = A[i];
         int j = i - 1;
-        while (j >= 0 && A[j] > temp) {
+        while (j >= 1 && A[j] > temp) {
             A[j + 1] = A[j];
             j = j - 1;
         }
